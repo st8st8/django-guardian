2259509b996f021e66e25d716b1d370e24068643
Jonathan Laver <jonathan.laver@mycoracle.com>
2014-06-26 16:52:05 +0100

    Add GetObjectsForOrganization

--- guardian/shortcuts.py
+++ guardian/shortcuts.py
@@ -612,3 +612,119 @@ def get_objects_for_group(group, perms, klass=None, any_perm=False):
 
     objects = queryset.filter(pk__in=pk_list)
     return objects
+    
+
+def get_objects_for_organization(organization, perms, klass=None, any_perm=False):
+    """
+    Returns queryset of objects for which a given ``organization`` has *all*
+    permissions present at ``perms``.
+
+    :param organization: ``organization`` instance for which objects would be returned.
+    :param perms: single permission string, or sequence of permission strings
+      which should be checked.
+      If ``klass`` parameter is not given, those should be full permission
+      names rather than only codenames (i.e. ``auth.change_user``). If more than
+      one permission is present within sequence, their content type **must** be
+      the same or ``MixedContentTypeError`` exception would be raised.
+    :param klass: may be a Model, Manager or QuerySet object. If not given
+      this parameter would be computed based on given ``params``.
+    :param any_perm: if True, any of permission in sequence is accepted
+
+    :raises MixedContentTypeError: when computed content type for ``perms``
+      and/or ``klass`` clashes.
+    :raises WrongAppError: if cannot compute app label for given ``perms``/
+      ``klass``.
+
+    Example:
+
+    Let's assume we have a ``Task`` model belonging to the ``tasker`` app with
+    the default add_task, change_task and delete_task permissions provided
+    by Django::
+
+        >>> from guardian.shortcuts import get_objects_for_organization
+        >>> from tasker import Task
+        >>> organization = organization.objects.create('some organization')
+        >>> task = Task.objects.create('some task')
+        >>> get_objects_for_organization(organization, 'tasker.add_task')
+        []
+        >>> from guardian.shortcuts import assign_perm
+        >>> assign_perm('tasker.add_task', organization, task)
+        >>> get_objects_for_organization(organization, 'tasker.add_task')
+        [<Task some task>]
+
+    The permission string can also be an iterable. Continuing with the previous example:
+        >>> get_objects_for_organization(organization, ['tasker.add_task', 'tasker.delete_task'])
+        []
+        >>> assign_perm('tasker.delete_task', organization, task)
+        >>> get_objects_for_organization(organization, ['tasker.add_task', 'tasker.delete_task'])
+        [<Task some task>]
+
+    """
+    if isinstance(perms, basestring):
+        perms = [perms]
+    ctype = None
+    app_label = None
+    codenames = set()
+
+    # Compute codenames set and ctype if possible
+    for perm in perms:
+        if '.' in perm:
+            new_app_label, codename = perm.split('.', 1)
+            if app_label is not None and app_label != new_app_label:
+                raise MixedContentTypeError("Given perms must have same app "
+                    "label (%s != %s)" % (app_label, new_app_label))
+            else:
+                app_label = new_app_label
+        else:
+            codename = perm
+        codenames.add(codename)
+        if app_label is not None:
+            new_ctype = ContentType.objects.get(app_label=app_label,
+                permission__codename=codename)
+            if ctype is not None and ctype != new_ctype:
+                raise MixedContentTypeError("ContentType was once computed "
+                    "to be %s and another one %s" % (ctype, new_ctype))
+            else:
+                ctype = new_ctype
+
+    # Compute queryset and ctype if still missing
+    if ctype is None and klass is None:
+        raise WrongAppError("Cannot determine content type")
+    elif ctype is None and klass is not None:
+        queryset = _get_queryset(klass)
+        ctype = ContentType.objects.get_for_model(queryset.model)
+    elif ctype is not None and klass is None:
+        queryset = _get_queryset(ctype.model_class())
+    else:
+        queryset = _get_queryset(klass)
+        if ctype.model_class() != queryset.model:
+            raise MixedContentTypeError("Content type for given perms and "
+                "klass differs")
+
+    # At this point, we should have both ctype and queryset and they should
+    # match which means: ctype.model_class() == queryset.model
+    # we should also have ``codenames`` list
+
+    # Now we should extract list of pk values for which we would filter queryset
+    organization_model = get_organization_obj_perms_model(queryset.model)
+    organizations_obj_perms_queryset = (organization_model.objects
+        .filter(organization=organization)
+        .filter(permission__content_type=ctype)
+        .filter(permission__codename__in=codenames))
+    if organization_model.objects.is_generic():
+        fields = ['object_pk', 'permission__codename']
+    else:
+        fields = ['content_object__pk', 'permission__codename']
+    organizations_obj_perms = organizations_obj_perms_queryset.values_list(*fields)
+    data = list(organizations_obj_perms)
+
+    keyfunc = lambda t: t[0] # sorting/organizationing by pk (first in result tuple)
+    data = sorted(data, key=keyfunc)
+    pk_list = []
+    for pk, organization in groupby(data, keyfunc):
+        obj_codenames = set((e[1] for e in organization))
+        if any_perm or codenames.issubset(obj_codenames):
+            pk_list.append(pk)
+
+    objects = queryset.filter(pk__in=pk_list)
+    return objects
