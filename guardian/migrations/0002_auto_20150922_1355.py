bb0d57fcdb9f1445f36d956ae6a8450b15a78eaa
Steve Bradshaw <steve@pcfive.co.uk>
2019-08-20 14:08:58 +0100

    On delete

--- guardian/migrations/0002_auto_20150922_1355.py
+++ guardian/migrations/0002_auto_20150922_1355.py
@@ -20,8 +20,8 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('object_pk', models.CharField(max_length=255, verbose_name='object ID')),
                 ('permission_expiry', models.DateTimeField(null=True, blank=True)),
-                ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
-                ('organization', models.ForeignKey(to='organizations.Organization')),
+                ('content_type', models.ForeignKey(to='contenttypes.ContentType', on_delete=models.CASCADE)),
+                ('organization', models.ForeignKey(to='organizations.Organization', on_delete=models.CASCADE)),
                 ('permission', models.ForeignKey(to='auth.Permission')),
             ],
         ),
@@ -40,3 +40,4 @@ class Migration(migrations.Migration):
             unique_together=set([('organization', 'permission', 'object_pk')]),
         ),
     ]
+
